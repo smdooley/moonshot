4680855480fa96548def9f5235ff2b433a552af9
Sean Dooley <s.dooley@ctidigital.com>
2024-11-18 11:34:18 +0000

    formatting our mission view - theme

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -29,20 +29,32 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
-
+                                    .padding()
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundStyle(.white)
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
                 }
+                .padding([.horizontal, .vertical])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
         }
     }
 }
