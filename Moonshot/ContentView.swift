3d10043df824579b67425c54d6d89c69ea6335d5
Sean Dooley <s.dooley@ctidigital.com>
2024-11-18 11:18:21 +0000

    using generics to load any kind of codable data

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
         VStack {
-            Text(String(astronauts.count))
+            Text("Astronauts: \(astronauts.count)")
+            Text("Missions: \(missions.count)")
         }
         .padding()
     }
