e180e5947d812323833c4a194caca06ca3e34420
Sean Dooley <s.dooley@ctidigital.com>
2024-11-19 14:29:19 +0000

    finishing up with one last view

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -43,7 +43,7 @@ struct MissionView: View {
                         HStack {
                             ForEach(crew, id: \.role) { crewMember in
                                 NavigationLink {
-                                    Text("Astronaut details")
+                                    AstronautView(astronaut: crewMember.astronaut)
                                 } label: {
                                     HStack {
                                         Image(crewMember.astronaut.id)
