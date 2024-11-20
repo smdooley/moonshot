f8bf3c4de549cde1099ede167e79b4b974b14552
Sean Dooley <s.dooley@ctidigital.com>
2024-11-20 14:12:11 +0000

    Upgrade it to use NavigationLink(value:) and Hashable conformance

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
@@ -22,44 +21,6 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            /*ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundStyle(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundStyle(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(.rect(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
-                        }
-                    }
-                }
-                .padding([.horizontal, .vertical])
-            }
-            .navigationTitle("Moonshot")
-            .background(.darkBackground)
-            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)*/
-            
             Group {
                 if (showingGrid) {
                     GridLayoutView(astronauts: astronauts, missions: missions)
@@ -76,6 +37,9 @@ struct ContentView: View {
                     Image(systemName: showingGrid ? "list.bullet" : "square.grid.2x2")
                         .foregroundColor(.white)
                 }
+            }
+            .navigationDestination(for: Mission.self) { mission in
+                    MissionView(mission: mission, astronauts: astronauts)
             }
         }
         .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
--- Moonshot/ListLayoutView.swift
+++ Moonshot/ListLayoutView.swift
@@ -13,9 +13,7 @@ struct ListLayoutView: View {
     
     var body: some View {
         List(missions) { mission in
-            NavigationLink {
-                MissionView(mission: mission, astronauts: astronauts)
-            } label: {
+            NavigationLink(value: mission) {
                 HStack {
                     Image(mission.image)
                         .resizable()
